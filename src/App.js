e681dc95a389d3d9ffb10830427a0e5929528fd6
J1MMM <jimuelbaraero00@gmail.com>
2022-09-29 21:14:04 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="my-portfolio/" element={<Layout/>}>
+        <Route path="/my-portfolio/" element={<Layout/>}>
           <Route index element={<Home/>} />
-          <Route path="about" element={<About/>} />
-          <Route path="skills" />
-          <Route path="projects" />
-          <Route path="contact" />
+          <Route path="/my-portfolio/about" element={<About/>} />
+          <Route path="/my-portfolio/skills" />
+          <Route path="/my-portfolio/projects" />
+          <Route path="/my-portfolio/contact" />
         </Route>
       </Routes>
     </>
