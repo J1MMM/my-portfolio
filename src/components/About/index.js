325e6150e842a217b74337014a559b9b9defd0e8
J1MMM <jimuelbaraero00@gmail.com>
2024-01-07 09:21:33 +0800

    123

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,48 +13,44 @@ const About = () => {
             return setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
-    return(
+    return (
         <>
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={aboutTitleArray}
-                        idx={12} /* 1.5sec deley */
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={aboutTitleArray}
+                            idx={12} /* 1.5sec deley */
                         />
-                </h1>
-                {/* <p>Ambitious web developer with 7 months of experience in front-end specially for aesthetic and responsive website design.</p> */}
-                <p>I work on personal projects using advanced and updated technologies to enhance my skill set</p>
-                <p>I currently taking bachelor's degree major in web and game development </p>
-                <p>I'm willing to learn on how to improve my skills by the help of other developers.</p>
-                <p>testing</p>
-                <p>mic test</p>
-            </div>
-            <div className='cube-cont'>
-                <div className='cube-spinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faReact} color="#61DBFB" />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
-                    </div>
-                    <div className='face5'>
-                        <FontAwesomeIcon icon={faPhp} color="#7377AD"/>
-                    </div>
-                    <div className='face6'>
-                        <FontAwesomeIcon icon={faNodeJs} color="#539E43"/>
+                    </h1>
+                    <p className='long-text'>I am a versatile full-stack developer with a comprehensive skill set that encompasses HTML, CSS, JavaScript, PHP, React, Angular, React Native, Expo, Node.js, and Express. Over the course of my independent development journey, I have excelled in creating responsive and visually compelling web applications. My proficiency extends to both front-end and back-end development, where I leverage frameworks like React for dynamic user interfaces and Node.js with Express for robust server-side applications.</p>
+                    <p className='long-text'>In addition to my web development expertise, I have successfully ventured into mobile app development using React Native and Expo, showcasing my commitment to staying at the forefront of cross-platform technologies. I pride myself on my ability to integrate databases seamlessly into applications, ensuring efficient data storage and retrieval. While my professional experiences have primarily been independent projects, I am enthusiastic about transitioning into collaborative team environments, where I can contribute my diverse skill set and passion for innovative development to collective success.</p>
+                </div>
+                <div className='cube-cont'>
+                    <div className='cube-spinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faReact} color="#61DBFB" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faPhp} color="#7377AD" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faNodeJs} color="#539E43" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Loader type='ball-pulse' color="#ffd700"/>
+            <Loader type='ball-pulse' color="#ffd700" />
         </>
     )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,9 +6,9 @@ import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
 import TagCloud from 'TagCloud';
 
-const Home = () =>{
+const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    
+
     const nameArray = "im,".split("");
     const jobArray = "web developer.".split("");
 
@@ -21,70 +21,70 @@ const Home = () =>{
     useEffect(() => {
         const container = '.Sphere';
         const texts = [
-        'HTML 5', 'CSS3', 'JavaScript',
-        'React', 'Node js', 'JSON',
-        'jQuery', 'SASS', 'Git',
-        'npm', 'Bootstrap',
-        'PHP', 'MongoDB', 'MySQL',
-        'Mongoose', 'Express', 'Tailwind',
-        'Laravel', 'Redux', 'Material-UI',
-        'GitHub', 'TypeScript' 
-    ];
-    const options1 = {
-        radius: 380,
-        maxSpeed: 'fast',
-        initSpeed: 'fast',
-        direction: 135,
-        keep: false
-    };
-    const options3 = {
-        radius: 180,
-        maxSpeed: 'fast',
-        initSpeed: 'fast',
-        direction: 135,
-        keep: false
-    };
+            'HTML 5', 'CSS3', 'JavaScript',
+            'React', 'Node js', 'JSON',
+            'jQuery', 'SASS', 'Git',
+            'npm', 'Bootstrap',
+            'PHP', 'MongoDB', 'MySQL',
+            'Mongoose', 'Express', 'Tailwind',
+            'Laravel', 'Redux', 'Material-UI',
+            'GitHub', 'TypeScript', 'Expo', 'React Native', 'Firebase'
+        ];
+        const options1 = {
+            radius: 380,
+            maxSpeed: 'fast',
+            initSpeed: 'fast',
+            direction: 135,
+            keep: false
+        };
+        const options3 = {
+            radius: 180,
+            maxSpeed: 'fast',
+            initSpeed: 'fast',
+            direction: 135,
+            keep: false
+        };
 
-    let options = window.innerWidth < 480 ? options3 : options1
-    TagCloud(container, texts, options);
+        let options = window.innerWidth < 480 ? options3 : options1
+        TagCloud(container, texts, options);
 
-}, [])
-    
-    return(
+    }, [])
+
+    return (
         <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _11`}>i</span>
-                    <span className={`${letterClass} _12`}>,</span>
-                    <br/>
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'</span>
-                    <span className={`${letterClass} _15`}>m</span>
-                    
-                    <img src={LogoJ} alt='logoj'/>
-                    
-                    <AnimatedLetters 
-                        letterClass={letterClass} 
-                        strArray={nameArray} 
-                        idx={16} 
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _11`}>i</span>
+                        <span className={`${letterClass} _12`}>,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'</span>
+                        <span className={`${letterClass} _15`}>m</span>
+
+                        <img src={LogoJ} alt='logoj' />
+
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={16}
                         />
-                    <br/>
-                    <AnimatedLetters 
-                        letterClass={letterClass} 
-                        strArray={jobArray} 
-                        idx={19} 
+                        <br />
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={jobArray}
+                            idx={19}
                         />
-                </h1>
-                <h2>Front End Developer / JavaScript / React js Developer</h2>
-                <Link to="/my-portfolio/contact" className='contact-button'>CONTACT ME</Link>
+                    </h1>
+                    <h2>Front End Developer / JavaScript / React js Developer</h2>
+                    <Link to="/my-portfolio/contact" className='contact-button'>CONTACT ME</Link>
+                </div>
+                <div className='sphere-cont tagcloud'>
+                    <span class='Sphere'></span>
+                </div>
             </div>
-            <div className='sphere-cont tagcloud'>
-                <span class='Sphere'></span>
-            </div>
-        </div>
-        <Loader type='ball-pulse' color="#ffd700"/>
+            <Loader type='ball-pulse' color="#ffd700" />
         </>
     )
 }
